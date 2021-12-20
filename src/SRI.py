529d3c393d5cd2a8cf2add940ea6e832c3333c73
JOlths <71876557+JOlths@users.noreply.github.com>
2021-12-20 17:39:48 +0100

    Added CSV file for testing

--- src/SRI.py
+++ src/SRI.py
@@ -1,6 +1,20 @@
 import numpy as numpy
 import pandas as pd
 
-def calculate_sri():
+def SRI_score(epochs_per_day=1440, sleep_data=pd.DataFrame()):
+	''' Calculate SRI score '''
 	pass
 
+def explore_data(sleep_data):
+	''' Exploring dataset for length, missing values, etc.'''
+	
+	# check if dataset is a python dataframe
+	assert type(sleep_data) == pd.DataFrame()
+
+	# 
+
+	pass
+
+
+if __name__ == "__main__":
+	pass
